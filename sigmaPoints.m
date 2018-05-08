6b83c0c299df824b48294e381838945e0b96c5d7
BViberg <bjorn.viberg@outlook.com>
2018-05-08 14:29:15 +0200

    Sigma points implemented the same way as TAs

--- sigmaPoints.m
+++ sigmaPoints.m
@@ -7,35 +7,52 @@ function [SP,W] = sigmaPoints(x, P, type)
 %   P           [n x n] Prior covariance
 %
 %Output:
-%   SP          [n x 2n+1] UKF, [n x 2n] CKF. Matrix with sigma points
-%   W           [1 x 2n+1] UKF, [1 x 2n] UKF. Vector with sigma point weights 
+%   SP          [n x 2n+1] matrix with sigma points
+%   W           [1 x 2n+1] vector with sigma point weights 
 %
 
     switch type        
         case 'UKF'
-            n=length(x);
-            W=zeros(1,2*n+1);
-            W(1)=1-n/3; 
-            W(2:end)=(1-W(1))/n;
-            SP=zeros(n,2*n+1);
-            SP(:,1)=x;
-            sqrtP=sqrtm(0.5*(P+P'));
-            for i=1:n
-                SP(:,i+1)=SP(:,1)+sqrt(n/(1-W(1)))*sqrtP(:,i);                
-                SP(:,n+i+1)=SP(:,1)-sqrt(n/(1-W(1)))*sqrtP(:,i);
+
+            % Dimension of state
+            n = length(x);
+
+            % Allocate memory
+            SP = zeros(n,2*n+1);
+
+            % Weights
+            W = [1-n/3 repmat(1/6,[1 2*n])];
+
+            % Matrix square root
+            sqrtP = sqrtm(P);
+
+            % Compute sigma points
+            SP(:,1) = x;
+            for i = 1:n
+                SP(:,i+1) = x + sqrt(1/2/W(i+1))*sqrtP(:,i);
+                SP(:,i+1+n) = x - sqrt(1/2/W(i+1+n))*sqrtP(:,i);
             end
+
         case 'CKF'
-            n=length(x);
-            W=zeros(1,2*n)+1/2/n;
-            
-            SP=zeros(n,2*n);
-            sqrtP=sqrtm(0.5*(P+P'));
-            for i=1:n
-                SP(:,i)=x+sqrt(n)*sqrtP(:,i);                
-                SP(:,n+i)=x-sqrt(n)*sqrtP(:,i);
+
+            % Dimension of state
+            n = length(x);
+
+            % Allocate memory
+            SP = zeros(n,2*n);
+
+            % Weights
+            W = repmat(1/2/n,[1 2*n]);
+
+            % Matrix square root
+            sqrtP = sqrtm(P);
+
+            % Compute sigma points
+            for i = 1:n
+                SP(:,i) = x + sqrt(n)*sqrtP(:,i);
+                SP(:,i+n) = x - sqrt(n)*sqrtP(:,i);
             end
+
         otherwise
             error('Incorrect type of sigma point')
     end
-
-end
